c7c00148204c8f78b56dec2a2be40e2e444ac695
Lara <laravarrenti@gmail.com>
2025-06-12 15:53:08 -0300

    Modificacion tipos de datos Alumno

--- src/entities/alumno.entity.ts
+++ src/entities/alumno.entity.ts
@@ -1,11 +1,11 @@
-export class Alumno{
-    constructor(
-        public dni: number,
-        public nombre: Date,
-        public apellido: number,
-        public direccion: string,
-        public telefono: string,
-        public email: number,
-        public legajo: number,     
-    ){}
+export class Alumno {
+  constructor(
+    public dni: string,
+    public nombre: string,
+    public apellido: string,
+    public direccion: string,
+    public telefono: number,
+    public mail: string,
+    public legajo: string //ver si es opcional
+  ) {}
 }
