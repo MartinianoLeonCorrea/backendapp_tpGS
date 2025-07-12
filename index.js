3831a4c337a173ded456e9ce2935890850c24490
MartinianoLeonCorrea <correamartinianoleon@gmail.com>
2025-07-12 18:52:09 -0300

    Adapto la pagina principal al index.html

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+// Librerías
 require('dotenv').config()
-
 const http = require ("http")
+const fs = require('fs')
 
+// Configuración del controlador de peticiones
 function requestController(req, res) {
   const URL = req.url
   const METHOD = req.method
   console.log({ URL, METHOD })
   if ( METHOD === 'GET' && URL === '/' ){
     res.setHeader("Content-type", "text/html; charset=utf-8")
-    res.write("<h1>Bienvenido a la página principal de la aplicación</h1>")
-    res.end()
+    fs.readFile('../frontendapp_TPGS/index.html', function(err, file){
+      if(err) {
+        console.log("Hubo un error")
+      }
+      res.write(file)
+      res.end()
+    })
     return
   }
   if ( METHOD === 'GET' && URL === '/about' ){
@@ -25,9 +32,7 @@ function requestController(req, res) {
 
 // Configuración del servidor
 const server = http.createServer(requestController)
-
 const PORT = process.env.PORT
-
 server.listen(PORT, function () {
   console.log("Apliación corriendo en puerto: " + PORT)
 })
