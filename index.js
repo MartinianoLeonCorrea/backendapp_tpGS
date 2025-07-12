a119bafbc62cc1243ff927e0e11f82931143b7ac
MartinianoLeonCorrea <correamartinianoleon@gmail.com>
2025-07-12 18:34:03 -0300

    Configurando request y response

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ require('dotenv').config()
 
 const http = require ("http")
 
-function requestController() {
-  console.log("Request recibida")
+function requestController(req, res) {
+  const URL = req.url
+  const METHOD = req.method
+  console.log({ URL, METHOD })
+  if ( METHOD === 'GET' && URL === '/' ){
+    res.setHeader("Content-type", "text/html; charset=utf-8")
+    res.write("<h1>Bienvenido a la página principal de la aplicación</h1>")
+    res.end()
+    return
+  }
+  if ( METHOD === 'GET' && URL === '/about' ){
+    res.setHeader("Content-type", "text/html; charset=utf-8")
+    res.write("<h1>Bienvenido a la sección about de la aplicación</h1>")
+    res.end()
+    return
+  }
+  res.setHeader("Content-type", "text/html; charset=utf-8")
+  res.write("<h1>Página no encontrada...</h1>")
+  res.end()
 }
 
 // Configuración del servidor
