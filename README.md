0757fa76d7388bac93e748031ea12b5629a460ff
Lara <laravarrenti@gmail.com>
2025-07-14 20:04:09 -0300

    Coneccion back y front usando express js

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Plataforma digital de gestión académica para institutos secundarios - Repositorio de Aplicación Backend
 
 ### Integrantes
-* 50851 - Achares, Jonatan Francisco Angel (Com304-2025)
-* 52858 - Correa, Martiniano León (Com304-2025)
-* 52572 - Varrenti, Lara (Com304-2025)
+
+- 50851 - Achares, Jonatan Francisco Angel (Com304-2025)
+- 52858 - Correa, Martiniano León (Com304-2025)
+- 52572 - Varrenti, Lara (Com304-2025)
 
 ### Información
+
 El repositorio donde se ubica la propuesta del trabajo completo se ubica por aquí:
-* [tpGestionSecundaria](https://github.com/MartinianoLeonCorrea/tpGestionSecundaria.git)
+
+- [tpGestionSecundaria](https://github.com/MartinianoLeonCorrea/tpGestionSecundaria.git)
--- index.js
+++ index.js
@@ -1,38 +1,21 @@
-// Librerías
-require('dotenv').config()
-const http = require ("http")
-const fs = require('fs')
+require('dotenv').config();
+//librería para manejar archivos estáticos más facilmente
+const express = require('express');
+const app = express();
+const port = process.env.PORT; //4000
 
-// Configuración del controlador de peticiones
-function requestController(req, res) {
-  const URL = req.url
-  const METHOD = req.method
-  console.log({ URL, METHOD })
-  if ( METHOD === 'GET' && URL === '/' ){
-    res.setHeader("Content-type", "text/html; charset=utf-8")
-    fs.readFile('../frontendapp_TPGS/index.html', function(err, file){
-      if(err) {
-        console.log("Hubo un error")
-      }
-      res.write(file)
-      res.end()
-    })
-    return
-  }
-  if ( METHOD === 'GET' && URL === '/about' ){
-    res.setHeader("Content-type", "text/html; charset=utf-8")
-    res.write("<h1>Bienvenido a la sección about de la aplicación</h1>")
-    res.end()
-    return
-  }
-  res.setHeader("Content-type", "text/html; charset=utf-8")
-  res.write("<h1>Página no encontrada...</h1>")
-  res.end()
-}
+//Servir archivos estáticos
+app.use(express.static('../frontendapp_tpGS'));
 
-// Configuración del servidor
-const server = http.createServer(requestController)
-const PORT = process.env.PORT
-server.listen(PORT, function () {
-  console.log("Apliación corriendo en puerto: " + PORT)
-})
+// Configurar las rutas
+app.get('/', function (req, res) {
+  res.send('Hola mundo');
+});
+app.get('/users', function (req, res) {
+  res.send([{ name: 'Lara' }, { name: 'Jonatan' }, { name: 'Martiniano' }]);
+});
+
+//Poner a escruchar la App en un puerto
+app.listen(port, function () {
+  console.log('Aplicación corriendo en el puerto: ' + port);
+});
