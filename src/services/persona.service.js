9fa46514e206a9bd28486554ad3052b8b9378ffa
Joni <jonatanachares@gmail.com>
2025-08-06 23:42:17 -0300

    Creacion archivo persona.services.js

---
+++ src/services/persona.service.js
@@ -0,0 +1,42 @@
+const { Persona } = require('../models');
+const { Op } = require('sequelize');
+
+class PersonaService {
+  async getAll() {
+    return await Persona.findAll();
+  }
+
+  async getById(id) {
+    return await Persona.findByPk(id);
+  }
+
+  async create(data) {
+    return await Persona.create(data);
+  }
+
+  async update(id, data) {
+    const persona = await Persona.findByPk(id);
+    if (!persona) return null;
+    await persona.update(data);
+    return persona;
+  }
+
+  async delete(id) {
+    const persona = await Persona.findByPk(id);
+    if (!persona) return null;
+    await persona.destroy();
+    return persona;
+  }
+
+  async searchByName(name) {
+    return await Persona.findAll({
+      where: {
+        nombre: {
+          [Op.like]: `%${name}%`
+        }
+      }
+    });
+  }
+}
+
+module.exports = new PersonaService();
