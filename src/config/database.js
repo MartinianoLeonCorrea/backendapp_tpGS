6f480e11909b77fa8da3c5c8cf1a721963bc5218
Lara <laravarrenti@gmail.com>
2025-09-09 17:29:45 -0300

    chore: configurar sequelize-cli para usar variables de entorno

--- src/config/database.js
+++ src/config/database.js
@@ -41,4 +41,11 @@ testConnection();
 
 module.exports = {
   sequelize,
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
 };
