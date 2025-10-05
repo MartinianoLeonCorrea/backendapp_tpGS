af1aea22f027be13a1c27676cc49c18f3b009841
Lara <laravarrenti@gmail.com>
2025-10-05 16:56:53 -0300

    refactor: eliminar los campos de marca de tiempo explícitos del modelo de Materia

--- src/modules/materia/materia.model.js
+++ src/modules/materia/materia.model.js
@@ -33,19 +33,6 @@ const Materia = sequelize.define(
         },
       },
     },
-
-    // Campos de auditoría automáticos (timestamps)
-
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      field: 'created_at',
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      field: 'updated_at',
-    },
   },
   {
     tableName: 'materias',
